d2fa89d7e1c2496d6a3fd3f483489f6deaa0ba1d
Thiago Calazans <thicalaza@gmail.com>
2021-07-26 16:25:51 -0300

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using FirstProject.Models;
+
+namespace FirstProject.Controllers
+{
+    public class HomeController : Controller
+    {
+       public ViewResult index()
+       {
+           int hour = DateTime.Now.Hour;
+           string viewModel = hour < 12 ? "Good Morning" : "Good Afternoon";
+           return View ("MyView", viewModel);
+       }
+    }
+}
